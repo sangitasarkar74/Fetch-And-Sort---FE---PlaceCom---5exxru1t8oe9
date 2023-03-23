0a00f9a860196fdbb654b9e5dcb1ab44c97234b5
sangitasarkar74 <sangitasarkar@gmail.com>
2023-03-23 19:47:26 +0530

    updated changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,65 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [sortAscending, setSortAscending] = useState(true);  
+  const [sortAscending, setSortAscending] = useState(true);
+
+  const fetchData = () => {
+    setIsLoading(true);
+    fetch("https://content.newtonschool.co/v1/pr/main/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .finally(() => setIsLoading(false));
+  };
+
+  const handleSort = () => {
+    setSortAscending(!sortAscending);
+    setUsers(
+      users.sort((a, b) => {
+        const nameA = a.name.length;
+        const nameB = b.name.length;
+        if (sortAscending) {
+          return nameA - nameB;
+        } else {
+          return nameB - nameA;
+        }
+      })
+    );
+  };
 
   return (
     <div id="main">
       <h2>User List</h2>
-      <button className="fetch-data-btn">Fetch User Data</button>
-      <button className="sort-btn">
-        "Sort by name length (ascending)"
-        "Sort by name length (descending)"
-      </button>
-      <p>Loading...</p>
-      <div className='users-section'>
-          <li>
-            <section className='id-section'></section>
-            <section className='name-email-section'>
-              <p className='name'>Name: </p>
-              <p className='email'>Email: </p>
-            </section>
-          </li>
-      </div>
-    </div>
-  )
-}
 
+      <button className="fetch-data-btn" onClick={fetchData}>
+        Fetch User Data
+      </button>
+      <button className="sort-btn" onClick={handleSort}>
+        {sortAscending
+          ? "Sort by name length (ascending)"
+          : "Sort by name length (descending)"}
+      </button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="users-section">
+          {users.map((user, index) => {
+            return (
+              <li key={index}>
+                <section className="id-section">{user.id}</section>
+                <section className="name-email-section">
+                  <p className="name">Name:{user.name}</p>
+                  <p className="email">Email:{user.email}</p>
+                </section>
+              </li>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
